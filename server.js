f27815297d55f659aaa58ca607adb1527e98f173
1234gk <144155567+1234gk@users.noreply.github.com>
2025-03-12 17:31:57 +0530

    add 1

--- server.js
+++ server.js
@@ -4,18 +4,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const axios = require("axios");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 
-// Error handling
 app.use((err, req, res, next) => {
   console.error('Error:', err);
   res.status(500).json({
@@ -23,7 +21,16 @@ app.use((err, req, res, next) => {
     message: 'Internal server error'
   });
 });
-
+app.get("/api/news", async (req, res) => {
+  try {
+      const response = await axios.get(
+          `https://newsapi.org/v2/everything?q=(natural disaster OR earthquake OR flood OR hurricane OR tsunami OR wildfire OR emergency)&language=en&sortBy=relevancy&pageSize=6&apiKey=79316c41264f41deaf480bbc126e3254`
+      );
+      res.json(response.data);
+  } catch (error) {
+      res.status(500).json({ error: "Failed to fetch news" });
+  }
+});
 // Connect to MongoDB
 mongoose.connect("mongodb+srv://dm:avs@cluster1.6oksn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
 .then(() => {
